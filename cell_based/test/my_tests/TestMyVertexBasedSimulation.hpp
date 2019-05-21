2e97962feede59cf380dfbd72a34e0906ab270d8
nm54yjx <yaojx@connect.hku.hk>
2019-05-21 21:10:31 +0800

    write MyCircleBoundaryCondition and use it in TestMyVertexBasedSimulation successfully.

--- cell_based/test/my_tests/TestMyVertexBasedSimulation.hpp
+++ cell_based/test/my_tests/TestMyVertexBasedSimulation.hpp
@@ -32,83 +32,40 @@ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
-/*
- *
- *  Chaste tutorial - this page gets automatically changed to a wiki page
- *  DO NOT remove the comments below, and if the code has to be changed in
- *  order to run, please check the comments are still accurate
- *
- *
- */
 
 #ifndef TESTMYVERTEXBASEDSIMULATION_HPP_
 #define TESTMYVERTEXBASEDSIMULATION_HPP_
 
-/*
- * = Examples showing how to create, run and visualize vertex-based simulations =
- *
- * EMPTYLINE
- *
- * == Introduction ==
- *
- * EMPTYLINE
- *
- * In this tutorial we show how Chaste can be used to create, run and visualize vertex-based simulations.
- * Full details of the mechanical model proposed by T. Nagai and H. Honda ("A dynamic cell model for
- * the formation of epithelial tissues", Philosophical Magazine Part B 81:699-719).
- *
- * EMPTYLINE
- *
- * == The test ==
- *
- * EMPTYLINE
- *
- * As in previous cell-based Chaste tutorials, we begin by including the necessary header files.
- */
 #include <cxxtest/TestSuite.h>
 #include "CheckpointArchiveTypes.hpp"
 #include "AbstractCellBasedTestSuite.hpp"
 
-/* The remaining header files define classes that will be used in the cell-based
- * simulation. We have encountered some of these header files in previous cell-based
- * Chaste tutorials. */
 #include "CellsGenerator.hpp"
 #include "OffLatticeSimulation.hpp"
 #include "TransitCellProliferativeType.hpp"
 #include "SmartPointers.hpp"
-/* The next header file defines the cell cycle model. */
 #include "UniformG1GenerationalCellCycleModel.hpp"
-/* The next two header files define a helper class for generating suitable meshes: one planar and one periodic. */
 #include "HoneycombVertexMeshGenerator.hpp"
+#include "ToroidalHoneycombVertexMeshGenerator.hpp"
 #include "CylindricalHoneycombVertexMeshGenerator.hpp"
-/* The next header file defines a vertex-based {{{CellPopulation}}} class.*/
 #include "VertexBasedCellPopulation.hpp"
-/* The next header file defines a force law for describing the mechanical interactions
- * between neighbouring cells in the cell population, subject to each vertex.
- */
 #include "NagaiHondaForce.hpp"
-/* This force law assumes that cells possess a "target area" property which determines the size of each
- * cell in the simulation. In order to assign target areas to cells and update them in each time step, we need
- * the next header file.
- */
 #include "SimpleTargetAreaModifier.hpp"
-/* The next header file defines a boundary condition for the cells.*/
 #include "PlaneBoundaryCondition.hpp"
-/* The next header file defines a cell killer, which specifies how cells are removed from the simulation.*/
 #include "PlaneBasedCellKiller.hpp"
 
-/* Finally, we include a header that enforces running this test only on one process. */
 #include "FakePetscSetup.hpp"
 
+#include "MyCircleBoundaryCondition.hpp"
 
-class TestRunningVertexBasedSimulationsTutorial : public AbstractCellBasedTestSuite
+class TestMyVertexBasedSimulation : public AbstractCellBasedTestSuite
 {
 public:
     
     void TestMonolayer()
     {
         
-        HoneycombVertexMeshGenerator generator(3, 3);    // Parameters are: cells across, cells up
+    	HoneycombVertexMeshGenerator generator(3, 3);    // Parameters are: cells across, cells up
         MutableVertexMesh<2,2>* p_mesh = generator.GetMesh();
 
         std::vector<CellPtr> cells;
@@ -120,7 +77,7 @@ public:
       
         OffLatticeSimulation<2> simulator(cell_population);
         simulator.SetOutputDirectory("MyVertexBasedSimulation");
-        simulator.SetEndTime(1.0);
+        simulator.SetEndTime(15.0);
         
         simulator.SetSamplingTimestepMultiple(50);
 
@@ -129,6 +86,13 @@ public:
        
         MAKE_PTR(SimpleTargetAreaModifier<2>, p_growth_modifier);
         simulator.AddSimulationModifier(p_growth_modifier);
+
+        c_vector<double, 2> centre = zero_vector<double>(2);
+        centre[0]=1.5;
+        centre[1]=1.5;
+        double radius = 2.2;
+        MAKE_PTR_ARGS(MyCircleBoundaryCondition, p_bc, (&cell_population, centre, radius));
+        simulator.AddCellPopulationBoundaryCondition(p_bc);
 
         simulator.Solve();
     }
